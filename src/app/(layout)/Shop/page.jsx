55eccb033d4beb79c115b93d7480a51e73791fa7
Azamatjon-developer <azamatergasev01@gmail.com>
2024-10-05 18:29:19 +0500

    ShopDone

--- src/app/(layout)/Shop/page.jsx
+++ src/app/(layout)/Shop/page.jsx
@@ -3,9 +3,22 @@ import React from 'react'
 const Shop = () => {
   return (
     <div>
-        <div className={"bg-shop-background bg-cover h-[90vh] relative"}>
-            Shop Page
+      <div
+        className={
+          'bg-shop-background bg-cover flex items-center justify-center h-[80vh]'
+        }
+      >
+        <div className={'flex justify-center items-center flex-col'}>
+          <h2
+            className={
+              ' text-[#000000] text-[48px] font-semibold'
+            }
+          >
+            Shop
+          </h2>
+          <p className={''}>Home Shop</p>
         </div>
+      </div>
     </div>
   )
 }
